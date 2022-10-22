e20925785e889dcb7294a9f15d4ddd7aeb46673f
Dani H <ctrl.alt.d@gmail.com>
2022-10-23 00:15:48 +0200

    The Game

---
+++ README.txt
@@ -0,0 +1,18 @@
+# FRESA
+
+Fressa és tan sols un joc. Fent fressa cal encaixar el quadrat petit dins el gros durant 5 segons seguits.
+
+Aquest joc el vaig picar ahir a la nit i no l'he provat mai en públic. Si m'animo el provo a la lightning talks de https://hacktoberfest.geeks.cat/.
+
+Qualsevol suggeriment, idea o ajuda per a la posada en escena és venbinguda. En principi la idea és partir de la configuració inicial i anar posant cada cop més amunt el quadrat gros (i potser fer-lo més petit)
+
+Si el proves i creus que es pot millorar alguna cosa (colors, text, confettis, ...), endavant amb les PRs!
+
+El codi està fet amb JS i p5.js
+
+Per executar-lo faig servir [Visual Studio Code](https://code.visualstudio.com/) amb el pluggin [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer)
+
+# License
+
+* The p5.js library is free software; you can redistribute it and/or modify it under the terms of the GNU Lesser General Public License as published by the Free Software Foundation, version 2.1.
+* Fressa té llicència MIT.
---
+++ sketch.js
@@ -0,0 +1,69 @@
+let mic;
+let background_size = 800;
+let fps = 30; 
+
+// player
+let player_size = 50;
+let player_height = 0;
+let dy = 5;
+
+// target
+let target_height = background_size / 5;
+let target_size = background_size / 4;
+
+// cronòmetre
+let seconcs_to_win = 5.;
+
+function setup(){
+  frameRate(fps);
+  let cnv = createCanvas(background_size, background_size);
+  cnv.mousePressed(userStartAudio);
+  textAlign(CENTER);
+  rectMode(CENTER);
+  mic = new p5.AudioIn();
+  mic.start();
+  textSize(100);
+}
+
+function draw(){
+  if (seconcs_to_win<0)
+  {
+    background(0,255,0);
+    return;
+  }
+  pintaSegonsQueFalten();
+  let aux_y = pillaElVolumAmbient();
+  pintaElTarget();
+  pintaElPlayer(aux_y);
+}
+
+function pintaElPlayer(aux_y) {
+  let isInside = (Math.abs(player_height - target_height) < (target_size - player_size) / 2);
+  let c = isInside ? color(0, 255, 0) : color(0, 0, 255);
+  fill(c);
+  seconcs_to_win = isInside ? seconcs_to_win - (1. / fps) : 5.;
+  player_height = player_height - dy;
+  if (player_height < 0)
+    player_height = 0;
+  if (aux_y > player_height)
+    player_height = aux_y;
+  rect(width / 2, background_size - player_height, player_size, player_size);
+}
+
+function pintaElTarget() {
+  fill(255, 204, 0);
+  rect(width / 2, background_size - target_height, target_size, target_size);
+}
+
+function pillaElVolumAmbient() {
+  micLevel = mic.getLevel();
+  let aux_y = map(micLevel, 0., 1., 0, background_size);
+  return aux_y;
+}
+
+function pintaSegonsQueFalten() {
+  background(0);
+  fill(255);
+  text(Math.trunc(seconcs_to_win), width / 2, 120);
+}
+
